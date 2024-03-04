1172efeb45c3be51fbefdf75635d74ddafe84889
PRANAV PRAVEEN <s26936@pjwstk.edu.pl>
2024-03-04 12:31:08 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Prompt the user to enter their name , surname , age
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+age = input("Enter your age: ")
+
+# Print a personalized greeting message
+print("Hello,", name + "!")
